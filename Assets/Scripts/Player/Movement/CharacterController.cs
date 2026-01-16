8325b14ab983582dba6d8037f20de3c560586412
corbenburgess <corbenburgess@hotmail.com>
2026-01-16 14:39:46 -0500

    Basic character controller has been made but the looking logic needs to be made and made into a netcode compatible movement.

--- Assets/Scripts/Player/Movement/CharacterController.cs
+++ Assets/Scripts/Player/Movement/CharacterController.cs
@@ -1,16 +1,62 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class CharacterController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Components")]
+    private CharacterController characterController;
+    private PlayerInputHandler inputHandler;
+
+    [Header("Variables")]
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float jumpPower = 5f;
+    [SerializeField] private float gravity = -9.81f;
+
+    private float verticalVelocity = 0f;
+
+    void Awake()
     {
-        
+        inputHandler = GetComponent<PlayerInputHandler>();
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveInput = inputHandler.Move;
+        bool jump = inputHandler.JumpPressed; // only true the frame button is pressed
+
+        HandleJump(jump);
+        MoveCharacter(moveInput);
+    }
+
+    private void HandleJump(bool jump)
+    {
+        // Apply a small downward force to keep CharacterController grounded detection consistent
+        if (characterController.isGrounded)
+        {
+            if (verticalVelocity < 0)
+                verticalVelocity = -0.1f;
+
+            if (jump)
+                verticalVelocity = jumpPower;
+        }
+        else
+        {
+            verticalVelocity += gravity * Time.deltaTime;
+        }
+    }
+
+
+    private void MoveCharacter(Vector2 moveInput)
+    {
+        // Convert input to world-relative movement
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        move *= walkSpeed;
+
+        // Add vertical velocity
+        move.y = verticalVelocity;
+
+        // Move character
+        characterController.Move(move * Time.deltaTime);
     }
 }
--- Assets/Scripts/Player/Movement/PlayerInputHandler.cs
+++ Assets/Scripts/Player/Movement/PlayerInputHandler.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class PlayerInputHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Expose input values
+    public Vector2 Move { get; private set; }
+    public bool JumpPressed { get; private set; }
+    public Key LastKeyPressed { get; private set; }
+
+    // Called automatically via PlayerInput Actions
+    public void OnMove(InputAction.CallbackContext context)
     {
-        
+        Move = context.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnJump(InputAction.CallbackContext context)
     {
-        
+        if (context.performed) JumpPressed = true;
+        else if (context.canceled) JumpPressed = false;
+
     }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("LOOKED");
+        }
+    }
+
+    public void OnAnyKey(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (context.control is KeyControl keyControl)
+                LastKeyPressed = keyControl.keyCode;
+        }
+    }
+
 }
